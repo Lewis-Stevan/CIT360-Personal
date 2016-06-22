e3fdf607314b3da8c4ef98027498bf9d9332e3c5
Halfspeed <Halfspeed@Steve>
2016-06-22 11:54:17 -0600

    Update

---
+++ StevanPortfolio/ApplicationControllerPattern/AddHandler.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class AddHandler<CalculatorActivity> implements Handler{
+	private double value1;
+	private double value2;
+	private double result;
+	private String resultString;
+	
+	public void handleIt(Object ... parameters) {
+		//Object is assigned activity
+		CalculatorActivity activity = (CalculatorActivity) parameters[0];
+		
+		//declare Textviews from textViews pass from Calculator
+		TableView txtInput = (TableView) parameters[1];
+		TableView txtSolution = (TableView) parameters[2];	
+
+		value1 = Double.parseDouble(txtInput.getText().toString());
+		value2 = Double.parseDouble(txtInput.getText().toString());
+		
+		result = value1 + value2;
+		
+		resultString = Double.toString(result);
+		
+		txtSolution.setText(resultString);			
+	 
+	    
+		txtInput.setText("");
+	}
+}
